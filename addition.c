5ad762d62185d2f5cee180df697f62359accdb45
Arjun_Gupta <gupta.arjun.8124@gmail.com>
2024-01-24 22:29:39 +0530

    Updated addition

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    printf("Enter your numbers:- ");
+    int a;
+    int b;
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    printf("The sum of the numbers is %d",c);
+}
